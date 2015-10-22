d73328c70674809d9593f308986decc64bbea594
Nicholas Glover <jacksaintjack@gmail.com>
2015-10-22 16:41:51 -0500

    made it look better

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 var answer = 0;
+var inputOne;
+var inputTwo;
 var calc = document.getElementById('calculateBtn').addEventListener('click', function(event){
   document.getElementById('answer').innerText = answer;
 });
 
 var add = document.getElementById('add').addEventListener('click', function(){
-  var inputOne = document.getElementById('num_one').value;
-  var inputTwo = document.getElementById('num_two').value;
+  inputOne = document.getElementById('num_one').value;
+  inputTwo = document.getElementById('num_two').value;
   answer = (parseFloat(inputOne) + parseFloat(inputTwo))
 });
 
 var subtact = document.getElementById('subtract').addEventListener('click', function(){
-  var inputOne = document.getElementById('num_one').value;
-  var inputTwo = document.getElementById('num_two').value;
+  inputOne = document.getElementById('num_one').value;
+  inputTwo = document.getElementById('num_two').value;
   answer = (parseFloat(inputOne) - parseFloat(inputTwo))
 });
 
 var divide = document.getElementById('divide').addEventListener('click', function(){
-  var inputOne = document.getElementById('num_one').value;
-  var inputTwo = document.getElementById('num_two').value;
+  inputOne = document.getElementById('num_one').value;
+  inputTwo = document.getElementById('num_two').value;
   answer = (parseFloat(inputOne) / parseFloat(inputTwo))
 });
 
 var multiple = document.getElementById('multiply').addEventListener('click', function(){
-  var inputOne = document.getElementById('num_one').value;
-  var inputTwo = document.getElementById('num_two').value;
+  inputOne = document.getElementById('num_one').value;
+  inputTwo = document.getElementById('num_two').value;
   answer = (parseFloat(inputOne) * parseFloat(inputTwo))
 });
